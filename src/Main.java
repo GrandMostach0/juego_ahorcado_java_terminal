b43d8c530ef9d773f3716da9f478f1e669438bc8
grandmostach0 <kreedlegend0@gmail.com>
2023-11-13 14:32:47 -0600

    modulo para mostrar palabras, cadena tipo char para converir palabra selccionada en caracteres: _

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         ajustesAhorcado  ahorcado = new ajustesAhorcado(5);
+        char palabraSeleccionada[] = new char[ahorcado.getPalabra().length()];
 
+        ahorcado.mostrarPalabra();
 
+        for(int i = 0; i < palabraSeleccionada.length; i++){
+            System.out.print(" _ ");
+        }
+
+        System.out.println("\n\nIntroduce una palabra");
+        String textoEntrada = scan.next();
 
     }
 }
--- src/ajustesAhorcado.java
+++ src/ajustesAhorcado.java
@@ -6,20 +6,21 @@ public class ajustesAhorcado {
 
     //array de las palabras a agregar
     private String listaPalabras[] = {"gato", "perro", "pajaro", "vaca", "cabra", "becerro"};
+    private char palabraSelccionada[];
     private int numVidas = 0;
+    String palabraElejida = "No se ha seleccionado ninguno";
     public ajustesAhorcado(int numeroVidas){
         this.numVidas = numeroVidas;
     }
 
     public String getPalabra(){
         int posicion = (int) (Math.random() * listaPalabras.length);
-        return listaPalabras[posicion];
+        palabraElejida = listaPalabras[posicion];
+        return palabraElejida;
     }
 
     public void mostrarPalabra(){
-        System.out.println(getPalabra());
+        System.out.println(palabraElejida);
     }
-
-
 
 }
