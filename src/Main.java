3e93865ffcb45ab33b4a1f0672f442dd13328cb3
grandmostach0 <kreedlegend0@gmail.com>
2023-11-13 14:57:33 -0600

    re-estructurando el codigo para verificar que haya completado todo

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,37 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         ajustesAhorcado  ahorcado = new ajustesAhorcado(5);
-        char palabraSeleccionada[] = new char[ahorcado.getPalabra().length()];
+        boolean compleado = false;
+        String palabraRandom = ahorcado.getPalabra();
+        char palabraSeleccionada[] = new char[palabraRandom.length()];
 
         ahorcado.mostrarPalabra();
 
-        for(int i = 0; i < palabraSeleccionada.length; i++){
-            System.out.print(" _ ");
+        for (int i = 0; i < palabraSeleccionada.length; i++) {
+            palabraSeleccionada[i] = '_';
+            System.out.print(" " + palabraSeleccionada[i] + " ");
         }
 
-        System.out.println("\n\nIntroduce una palabra");
-        String textoEntrada = scan.next();
+        while(!compleado){
+            System.out.println("\n\nIntroduce una letra");
+            char textoEntrada = scan.next().charAt(0);
+
+            for (int n = 0; n < palabraSeleccionada.length; n++){
+                if( palabraRandom.charAt(n)== textoEntrada){
+                    palabraSeleccionada[n] = textoEntrada;
+                }
+            }
+
+            for (int i = 0; i < palabraSeleccionada.length; i++) {
+                System.out.print(" " + palabraSeleccionada[i] + " ");
+            }
+
+            for(int k = 0; k < palabraSeleccionada.length; k++){
+                if(palabraSeleccionada[k] != '_'){
+                    compleado = true;
+                }
+            }
+        }
 
     }
 }
